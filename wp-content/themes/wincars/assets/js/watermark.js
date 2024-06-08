2cf9cbba5eea4062a6689ec5e76b6545dd787987
Petr <shevron.dev@gmail.com>
2024-06-08 18:21:15 +0300

    Alfa version

--- wp-content/themes/wincars/assets/js/watermark.js
+++ wp-content/themes/wincars/assets/js/watermark.js
@@ -1,6 +1,7 @@
 let uploads = document.getElementById('oploaud'),
 	imagesWrapper = document.getElementById('watermark-images'),
-	dwnldBtn = document.getElementById('get-wm-archive');
+	dwnldBtn = document.getElementById('get-wm-archive'),
+	readyToDownload = [];
 
 uploads.addEventListener('change', () => {
 	if (uploads.files.length > 0) {
@@ -23,32 +24,57 @@ dwnldBtn.addEventListener('click', async () => {
 	let imagesDiv = document.querySelectorAll('.watermark__img'),
 		imagesDivArr = Array.from(imagesDiv);
 
-	const promises = imagesDivArr.map(async (imageDiv) => {
-			html2canvas(imageDiv, {
-				scrollY: (window.pageYOffset * -1)
-			}).then(function (canvas) {
-				if (imagesDivArr.length == 1) {
-					const link = document.createElement('a');
-					link.download = 'collage.png';
-					link.href = canvas.toDataURL();
-					link.click();
-					link.delete;
-				}
-				if (imagesDivArr.length > 1) {
-					let zip = new JSZip(),
-						folder = zip.folder();
-				}
+	/**
+	 * Save image like it is, without zip
+	 * @param {Object} readyToDownload - an object with all the necessary data to save image (name and url)
+	 */
+	let saveSingleImage = (readyToDownload) => {
+		const link = document.createElement('a');
+		link.download = readyToDownload.name;
+		link.href = readyToDownload.imgDataUrl;
+		link.click();
+		link.delete;
+	}
+
+	/**
+	 * Save zip archive with images
+	 * @param {Array} readyToDownload - an array of objects with all the necessary data to save image (name and blob)
+	 */
+	let saveImageArchive = (readyToDownload) => {
+		let zip = new JSZip(),
+			folder = zip.folder();
+
+		readyToDownload.forEach((imageObj) => {
+			// console.log(imageObj);
+			// Назначение строки ниже пока не понял
+			// let imageFile = new File([imageBlob], imagesNames[index]);
+
+			folder.file(imageObj.name, imageObj.imgBlob);
+		})
+
+		folder.generateAsync({ type: "blob" }).then(content => saveAs(content, 'Wincars images'));
+	}
+
+	const promises = imagesDivArr.map(async (imageDiv, index) => {
+		html2canvas(imageDiv, {
+			scrollY: (window.pageYOffset * -1)
+		}).then(function (canvas) {
+			let imageObj = {
+				name: uploads.files[index].name,
+				imgDataUrl: canvas.toDataURL("image/jpeg"), //"image/jpeg"
+				// imgBlob:
+			}
+			canvas.toBlob((blob) => {
+				imageObj.imgBlob = blob;
 			})
-	})
-	const imagesBlobs = await Promise.all(promises);
-
-
-	imagesBlobs.forEach((imageBlob, index) => {
-		// Назначение строки ниже пока не понял
-		// let imageFile = new File([imageBlob], imagesNames[index]);
-		folder.file(uploads.files[index], imageBlob);
+			readyToDownload.push(imageObj);
+		})
 	})
 
-	// folder.generateAsync({ type: "blob" }).then(content => saveAs(content, 'Wincars images'));
-
+	if (readyToDownload.length == 1) {
+		saveSingleImage(readyToDownload[0])
+	}
+	if (readyToDownload.length > 1) {
+		saveImageArchive(readyToDownload)
+	}
 })
