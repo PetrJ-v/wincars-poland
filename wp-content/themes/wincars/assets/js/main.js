18766bd5943058f710fb14404022442c7594e340
Petr <shevron.dev@gmail.com>
2024-07-02 01:22:55 +0300

    Cars table 3 col

--- wp-content/themes/wincars/assets/js/main.js
+++ wp-content/themes/wincars/assets/js/main.js
@@ -6,9 +6,23 @@
 		}
 	};
 	$(window).load(function () {
-
 		$('#top-btn').on('click', () => {
 			document.navigateFunc($('body'));
 		})
+		// console.log(e.currentTarget);
+		$('.cars .car').on('mouseenter', function(){
+			$(this).toggleClass('hover');
+		})
+		$('.cars .car').on('mouseleave', function(){
+			$(this).toggleClass('hover');
+		})
+		// $('.cars .car').on('mouseleave', (e) => {
+		// 	let hiddenText = $(e.currentTarget).find('.hidden-text'),
+		// 		visibleText = $(e.currentTarget).find('.visible-text');
+
+		// 	visibleText.fadeIn('800');
+		// 	hiddenText.fadeOut('800');
+
+		// })
 	});
 })(jQuery);
