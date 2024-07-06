c390ae1524a68fcd066fb8bf47ada7e46375142a
Petr <shevron.dev@gmail.com>
2024-07-07 00:05:36 +0300

    Faq block

--- wp-content/themes/wincars/assets/js/home.js
+++ wp-content/themes/wincars/assets/js/home.js
@@ -40,5 +40,20 @@
 				},
 			});
 		}
+
+		// Faq block
+		$('.question').on('click', (e) => {
+			let faqItem = $(e.currentTarget).closest('.faq__item'),
+				faqAnswer = faqItem.find('.faq__item-answer');
+
+			if (!faqItem.hasClass('faq__item--opened')) {
+				faqItem.addClass('faq__item--opened');
+				faqAnswer.slideDown(1000);
+			}
+			else {
+				faqItem.removeClass('faq__item--opened');
+				faqAnswer.slideUp(1000);
+			}
+		})
 	});
 })(jQuery);
