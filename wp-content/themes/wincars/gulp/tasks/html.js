5bea2a74d2571e92f4d863e11f3b5d393e37b4c2
Petr <shevron.dev@gmail.com>
2024-07-22 01:46:26 +0300

    Bug fix and add verions to build task

--- wp-content/themes/wincars/gulp/tasks/html.js
+++ wp-content/themes/wincars/gulp/tasks/html.js
@@ -1,4 +1,5 @@
 import fileinclude from "gulp-file-include";
+import versonNumber from "gulp-version-number";
 
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
@@ -11,28 +12,25 @@ export const html = () => {
 		.pipe(fileinclude())
 		.pipe(app.plugins.replace(/@img\//g, 'img/'))
 		.pipe(app.plugins.replace(/@js\//g, 'js/'))
-		// .pipe(
-		// 	app.plugins.if(
-		// 		app.isBuild,
-		// 		webpHtmlNosvg()
-		// 	)
-		// )
-		// .pipe(
-		// 	versonNumber({
-		// 		'value': '%DT%',
-		// 		'append': {
-		// 			'key': '_v',
-		// 			'cover': 0,
-		// 			'to': [
-		// 				'css',
-		// 				'js',
-		// 			]
-		// 		},
-		// 		'output': {
-		// 			'file': 'gulp/version.json'
-		// 		}
-		// 	})
-		// )
+		.pipe(
+			app.plugins.if(
+				app.isBuild,
+				versonNumber({
+					'value': '%DT%',
+					'append': {
+						'key': '_v',
+						'cover': 0,
+						'to': [
+							'css',
+							'js',
+						]
+					},
+					'output': {
+						'file': 'gulp/version.json'
+					}
+				})
+			)
+		)
 		.pipe(app.gulp.dest(app.path.build.html))
 		.pipe(app.plugins.browserSync.stream());
 }
