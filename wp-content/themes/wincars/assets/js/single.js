7f923c8fcd9ae6450eb9ba42b1b7b2dc51d268f5
Petr <shevron.dev@gmail.com>
2024-07-21 17:42:59 +0300

    Post page

---
+++ wp-content/themes/wincars/assets/js/single.js
@@ -0,0 +1,32 @@
+(function ($) {
+	$(window).load(function () {
+		if ($('.previews-slider')) {
+			var previewSlider = new Swiper(".previews-slider", {
+				slidesPerView: 1,
+				spaceBetween: 20,
+				// centeredSlides: true,
+				initialSlide: 1,
+				watchOverflow: true,
+				navigation: {
+					nextEl: ".swiper-button-next",
+					prevEl: ".swiper-button-prev",
+				},
+				keyboard: {
+					enabled: true,
+					onlyInViewport: true,
+				},
+				breakpoints: {
+					575.98: {
+						slidesPerView: 2,
+					},
+					// 640.98: {
+					// 	slidesPerView: 2,
+					// },
+					991.98: {
+						slidesPerView: 3,
+					},
+				},
+			});
+		}
+	});
+})(jQuery);
