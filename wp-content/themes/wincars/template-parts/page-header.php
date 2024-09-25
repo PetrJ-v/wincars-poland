82f8f8278be9fa22851e6ed0621dac3c8a703d5d
Petr <shevron.dev@gmail.com>
2024-09-25 09:43:21 +0300

    Move header top line to template part

--- wp-content/themes/wincars/template-parts/page-header.php
+++ wp-content/themes/wincars/template-parts/page-header.php
@@ -2,36 +2,12 @@
 	<div class="header">
 		<div class="header-container">
 			<div class="header-media active" data-function="toggleClassName" data-anim="true">
-				<div class="header-menu-wrapper">
-					<?php get_template_part('template-parts/header-menu-btn'); ?>
-					<?php
-					wp_nav_menu([
-						'theme_location'  => 'top',
-						'container'       => 'nav',
-						'container_class' => 'header-menu',
-						'echo'            => true,
-						'fallback_cb'     => 'wp_page_menu',
-						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-						'depth'           => 1,
-					]);
-					?>
-					<a href="<?php echo get_home_url(); ?>" class="header-logo img-wrapper">
-						<img src="<?php echo _TEMPLATEPATH; ?>/assets/img/logo-black-w.svg" alt="Wincars logo">
-					</a>
-					<!-- <a href="<?php echo get_home_url(); ?>" class="header-logo img-wrapper">
-						<img src="<?php echo _TEMPLATEPATH; ?>/assets/img/logo-white-b.svg" alt="Wincars logo">
-					</a> -->
-				</div>
+				<?php get_template_part('template-parts/header-top-line'); ?>
 				<div class="header-media__img">
 					<?php
 					$header_image = get_field('header_image');
 					echo wp_get_attachment_image($header_image, 'full');
 					?>
-				</div>
-				<div class="header-media__cta">
-					<div class="header-media__cta-btn-wrapper">
-						<button class="header-media__cta-btn accent-btn open-popup" data-target="main-form">Остави запитване</button>
-					</div>
 				</div>
 			</div>
 		</div>
