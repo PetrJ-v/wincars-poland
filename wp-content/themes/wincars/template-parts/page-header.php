210167545f7d3f005aa7ef46d596be3bfef1cd30
Petr <shevron.dev@gmail.com>
2024-10-20 14:07:28 +0300

    Contacts upgrade temp point

--- wp-content/themes/wincars/template-parts/page-header.php
+++ wp-content/themes/wincars/template-parts/page-header.php
@@ -4,10 +4,22 @@
 			<div class="header-media active" data-function="toggleClassName" data-anim="true">
 				<?php get_template_part('template-parts/header-top-line'); ?>
 				<div class="header-media__img">
-					<?php
-					$header_image = get_field('header_image');
-					echo wp_get_attachment_image($header_image, 'full');
-					?>
+					<?php $header_type = get_field('header_type'); ?>
+					<?php if ($header_type === 'image') : ?>
+						<?php
+							$header_image = get_field('header_image');
+							echo wp_get_attachment_image($header_image, 'full');
+						?>
+					<?php elseif ($header_type === 'video') : ?>
+						<?php
+							$header_video = get_field('header_video');
+							$desktop_video = $header_video['video_desktop'];
+							$desktop_video_poster = $header_video['video_poster_desktop'];
+						?>
+						<!-- <video preload="none" autoplay="" muted="" playsinline="" loop="" poster="<?php echo $video_poster_desktop; ?>" src="<?php echo $video_url_desktop; ?>"></video> -->
+						<video  muted="" width="1365" height="450" playsinline="" loop="" poster="<?php echo $desktop_video_poster; ?>" src="<?php echo $desktop_video; ?>"></video>
+					<?php endif; ?>
+
 				</div>
 			</div>
 		</div>
