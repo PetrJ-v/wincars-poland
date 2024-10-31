8d91caa0fdaed7a50c08e4ca85e4f01ca35ac657
Petr <shevron.dev@gmail.com>
2024-10-31 14:20:17 +0200

    change domain in tracking.js

--- wp-content/themes/wincars/assets/js/tracking.js
+++ wp-content/themes/wincars/assets/js/tracking.js
@@ -71,7 +71,7 @@
 					if (responseData.status != 'error') {
 
 						let containerNumnber = responseData.container_number;
-						window.location = 'https://wincars.local/tracking/?' + containerNumnber;
+						window.location = 'https://www.wincars.bg/tracking/?' + containerNumnber;
 					} else {
 						$('.tracking-form-message').fadeIn();
 						console.log(responseData.error);
