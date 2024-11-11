0565c3371c082db2532456c5bd228f7b0f2d79cb
Petr <shevron.dev@gmail.com>
2024-11-11 03:04:03 +0200

    Test extened data from cargo

--- wp-content/themes/wincars/templates/tracking.php
+++ wp-content/themes/wincars/templates/tracking.php
@@ -32,6 +32,74 @@
 			</div> -->
 			<button class="accent-btn test-btn">Получить данные</button>
 		</div>
+		<div class="container">
+			<?php
+			// API URL
+			$url = "https://www.cargoloop.com:8081/SVehicle.asmx";
+
+			// SOAP XML for sending
+			// $vin = "1FA6P0HD2E5369469";
+			$vin = "KM8KN4DE0RU263503";
+			$user_name = 'wincars';
+			$password = 'McLaren2!24';
+
+			// SOAP request
+			$xml_data = '<?xml version="1.0" encoding="utf-8"?>
+					<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+					<soap:Body>
+						<GetVehicleDetailsByAccount xmlns="http://www.cargoloop.com/">
+						<VINorLot>' . $vin . '</VINorLot>
+						<Username>' . $user_name . '</Username>
+						<Password>' . $password . '</Password>
+						</GetVehicleDetailsByAccount>
+					</soap:Body>
+					</soap:Envelope>';
+
+			// Headers for SOAP-request
+			$headers = [
+				"Content-Type: text/xml; charset=utf-8",
+				"Content-Length: " . strlen($xml_data),
+				"SOAPAction: \"http://www.cargoloop.com/GetVehicleDetailsByAccount\"",
+			];
+			// cURL init
+			$ch = curl_init($url);
+
+			// Setup cURL
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_data);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+			// Sending request and gettong responce
+			$response = curl_exec($ch);
+			$err = curl_error($ch);
+
+			if ($err) {
+				echo "Error cURL: " . $err;
+			} else {
+				// Parse XML-response
+				$xml = simplexml_load_string($response);
+
+				// Namespace for correct access to elements
+				$namespaces = $xml->getNamespaces(true);
+
+				// Go to response body
+				$body = $xml->children($namespaces['soap'])->Body;
+				$response_data = $body->children($namespaces[''])->GetVehicleDetailsByAccountResponse;
+				$result = $response_data->GetVehicleDetailsByAccountResult;
+
+				$auction = (string) $result->Auction;
+
+				echo "Auction: " . (string) $result->Auction . "<br>";
+				echo "LotNumber: " . (string) $result->LotNumber . "<br>";
+				echo "Description: " . (string) $result->Description . "<br>";
+				echo "ContainerNumber: " . (string) $result->ContainerNumber . "<br>";
+				echo "ShippedDate: " . (string) $result->ShippedDate;
+			}
+
+			curl_close($ch);
+			?>
+		</div>
 	</main>
 
 	<?php get_footer(); ?>
